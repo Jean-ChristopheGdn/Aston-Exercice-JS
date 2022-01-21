2219f391f9584ab818acca92492af5e406eca402
Jean-ChristopheGdn <jeanchristophe.gendron@gmail.com>
2022-01-21 14:25:40 +0100

    :recycle: split fizzbuzz ex3 into 2 functions

--- Exercices/Exercice3/exercice3.js
+++ Exercices/Exercice3/exercice3.js
@@ -4,21 +4,22 @@ let fizzBuzzSection = document.getElementById("fizzBuzz");
 
 // Function FizzBuzz: Display Fizz if %3, Buzz if %5 and FizzBuzz for both.
 
-function fizzBuzz() {
+function displayFizzBuzz() {
     let number = 100;
     for (let i = 1; i <= number; i++) {
-
-        if (i % 3 == 0 && i % 5 == 0) {
+        let check = checkFizzBuzz(i); 
+        console.log(check);      
+        if (check === "FizzBuzz") {
             let display = document.createElement("div");
             display.innerHTML += i + ": FizzBuzz<br>";
             fizzBuzzSection.appendChild(display);
         }
-        else if (i % 3 == 0) {
+        else if (check === "Fizz") {
             let display = document.createElement("div");
             display.innerHTML += i + ": Fizz<br>";
             fizzBuzzSection.appendChild(display);
         } 
-        else if (i % 5 == 0) {
+        else if (number === "Buzz") {
             let display = document.createElement("div");
             display.innerHTML += i + ": Buzz<br>";
             fizzBuzzSection.appendChild(display);
@@ -30,4 +31,16 @@ function fizzBuzz() {
         }
     }
 }
-fizzBuzz();
+function checkFizzBuzz(number){
+    if (number % 3 == 0 && number % 5 == 0) {
+        return "FizzBuzz"
+    }
+    else if (number % 3 == 0) {
+        return "Fizz"
+    } 
+    else if (number % 5 == 0) {
+        return "Buzz"
+    } 
+}
+
+displayFizzBuzz();
